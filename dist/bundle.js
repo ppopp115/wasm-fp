54b15df071ed620f09d9e54582ab9292a831251b
anonyco <wowzeryest@gmail.com>
2017-12-30 13:35:59 -0500

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,8 +1,8 @@
 "use strict";
 window.ReactDOM=React.__SECRET_DOM_DO_NOT_USE_OR_YOU_WILL_BE_FIRED; // load ReactDOM
 (()=>{
-	let old_preventDefault = EventTarget.prototype.preventDefault;
-	EventTarget.prototype.preventDefault = function() {
+	let old_preventDefault = Event.prototype.preventDefault;
+	Event.prototype.preventDefault = function() {
 		if (this.type !== "mousewheel" && this.type !== "touchmove" && type !== "scroll" && this.type !== "touchstart")
 			old_preventDefault.call(this);
 	};
